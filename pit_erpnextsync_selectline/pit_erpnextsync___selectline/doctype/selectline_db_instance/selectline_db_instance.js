1914d3dbee8014e6b7726f8f3d597e2936582532
Philipp Geiselmayr <t06@pit-it.at>
2025-12-29 18:57:04 +0000

    291225-4

--- pit_erpnextsync_selectline/pit_erpnextsync___selectline/doctype/selectline_db_instance/selectline_db_instance.js
+++ pit_erpnextsync_selectline/pit_erpnextsync___selectline/doctype/selectline_db_instance/selectline_db_instance.js
@@ -10,8 +10,46 @@ frappe.ui.form.on("Selectline DB Instance", {
     },
     import_json_file(frm) {
         import_json_file(frm);
+    },
+    start_import(frm) {
+        start_import_action(frm);
+    },
+    reset(frm) {
+        frm.set_value("types_to_import", "");
+        frm.set_value("amount_of_data_rows", 0);
     }
 });
+
+
+// start import button action
+function start_import_action(frm){
+    
+    // check types
+    let types_str = frm.doc.types_to_import
+    let types_list = [];
+    if (types_str){
+        types_list = types_str.split(",");
+    }
+
+    let data_rows_int = frm.doc.amount_of_data_rows
+
+    // call import
+    frappe.dom.freeze(__("creating background jobs for import..."));
+    frappe.call({
+        method: "pit_erpnextsync_selectline.scripts.import.start_import",
+        args:{
+            "instance": frm.doc.name,
+            "types_str": types_list,
+            "top": data_rows_int
+        },
+        callback: function(r){
+            frappe.dom.unfreeze();
+            frappe.msgprint("Background jobs were created. Import runs in background.")
+        }
+    });
+    
+
+}
 
 
 // import json mapping file
