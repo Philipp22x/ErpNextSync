f37cc60678b8d94bc49ee60650e8028399e41bc5
Philipp Geiselmayr <t06@pit-it.at>
2026-03-07 10:29:37 +0000

    added workspace

--- pit_erpnextsync/pit_erpnextsync/doctype/pit_erpnextsync_settings/pit_erpnextsync_settings.py
+++ pit_erpnextsync/pit_erpnextsync/doctype/pit_erpnextsync_settings/pit_erpnextsync_settings.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, PIT IT GmbH and contributors
+# Copyright (c) 2026, PIT IT GmbH and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class PitERPNextSyncSettings(Document):
+class PitErpNextSyncSettings(Document):
 	pass
